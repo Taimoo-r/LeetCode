0804d741adabd764a499c17aee16fc0324d8eff1
Taimoo-r <taimoorking23102004@gmail.com>
2024-05-30 16:39:06 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (78.50%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-         int n = word1.length() + word2.length();
-        int j = 0, u = 0;
-        string res;
-        res.reserve(n);  
-        
-        for(int i = 0; i < n; i++) {
-            if(i % 2 == 0) {
-                if(j < word1.length()) {
-                    res += word1[j++];
-                } else if(u < word2.length()) {
-                    res += word2[u++];
-                }
-            } else {
-                if(u < word2.length()) {
-                    res += word2[u++];
-                } else if(j < word1.length()) {
-                    res += word1[j++];
-                }
-            }
+        string res="";
+        int i=0,j=0;
+        while(i<word1.length()&&j<word2.length()){
+            res+=word1[i++];
+            res+=word2[j++];
         }
-        
+        while(i<word1.length())
+        res+=word1[i++];
+        while(j<word2.length())
+        res+=word2[j++];
         return res;
     }
 };
