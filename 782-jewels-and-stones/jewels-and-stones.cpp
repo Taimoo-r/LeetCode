d2a10949cd4dd04aeb2558359e637e3bae72e6c3
Taimoo-r <taimoorking23102004@gmail.com>
2024-05-31 12:46:50 +0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (26.14%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int res=0;
-        for(int i=0;i<jewels.size();i++){
-            for(int j=0;j<stones.size();j++){
-                if(jewels[i]==stones[j])
-                res++;
-            }
+        unordered_map<char,char> hm;
+        for(auto j : jewels){
+            hm[j] = j;
+        }
+        for(auto s : stones){
+            if(hm.find(s)!=hm.end())
+            res++;
         }
         return res;
     }
