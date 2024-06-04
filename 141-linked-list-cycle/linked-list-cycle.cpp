9c94e63c9ca4a559d5df03fc0ff39d0b65c1cc64
Taimoo-r <taimoorking23102004@gmail.com>
2024-06-04 13:38:45 +0500

    Time: 16 ms (11.61%) | Memory: 15 MB (5.62%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -5,16 +5,14 @@ public:
             return false;
         }
         ListNode *temp = head;
-        std::unordered_map<ListNode*, bool> nodeSeen;
+        unordered_map<ListNode*, bool> nodeSeen;
         while (temp != NULL) {
-            // Check if we've seen this node before
             if (nodeSeen.find(temp) != nodeSeen.end()) {
-                return true; // Cycle detected
+                return true; 
             }
-            // Mark this node as seen
             nodeSeen[temp] = true;
             temp = temp->next;
         }
-        return false; // No cycle detected
+        return false;
     }
 };
