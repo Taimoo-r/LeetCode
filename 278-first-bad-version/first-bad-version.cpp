72821f6057b488d4d646b06d92bf431b44447157
Taimoo-r <taimoorking23102004@gmail.com>
2024-06-06 09:43:59 +0500

    Time: 2 ms (57.37%) | Memory: 7.3 MB (58.31%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -6,21 +6,15 @@ public:
     int firstBadVersion(int n) {
         if(isBadVersion(1))
         return 1;
-        long start = 1, end = n,prev,mid;
-        while(start<=end){
-            mid = (start+end)/2;
+        int start = 1, end = n,prev,mid;
+        while(start<end){
+            mid = start + (end-start)/2;
             if(isBadVersion(mid)){
-            prev = mid;
-            break;
+            end = mid;
             }
             else if(!isBadVersion(mid))
             start = mid+1;
        }
-       for(int i= prev-1;i>=1;i--){
-        if(!isBadVersion(i)){
-            return i+1;
-        }
-    }
-    return -1;
+    return start;
     }
 };
