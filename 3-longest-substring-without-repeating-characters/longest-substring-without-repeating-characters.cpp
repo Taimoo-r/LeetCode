c568bf56a5f00a5edd3a35d575f5eb36a9f5f76d
Taimoo-r <taimoorking23102004@gmail.com>
2024-07-06 10:24:13 +0500

    Time: 21 ms (33.35%) | Memory: 15.6 MB (12.67%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-//         int left,right,ans;
-//         left = right = ans = 0;
-//         int n = s.size();
-//         set<char> S;
-//         while(left < n && right < n){
-//             if(S.find(s[right])==S.end()){
-//                 S.insert(s[right]);
-//                 right++;
-//                 ans = max(ans,right-left);
-//                 }
-//             else{
-//                 S.erase(s[left]);
-//                 left++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-        unordered_set<char> se;
-        vector<unordered_set<char>> v;
-        int count = 0;
+        int left,right,ans;
+        left = right = ans = 0;
         int n = s.size();
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i ; j < n && se.find(s[j])==se.end();j++){
-                se.insert(s[j]);
+        set<char> S;
+        while(left < n && right < n){
+            if(S.find(s[right])==S.end()){
+                S.insert(s[right]);
+                right++;
+                ans = max(ans,right-left);
+                }
+            else{
+                S.erase(s[left]);
+                left++;
             }
-            v.push_back(se);
-            se.clear(); 
         }
-        int mx = 0;
-      for(int i = 0; i < v.size(); i++) {
-        int t = v[i].size();
-        mx = max(mx,t);
-    }
-        return mx;
+        return ans;
     }
 };
+//         unordered_set<char> se;
+//         vector<unordered_set<char>> v;
+//         int count = 0;
+//         int n = s.size();
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = i ; j < n && se.find(s[j])==se.end();j++){
+//                 se.insert(s[j]);
+//             }
+//             v.push_back(se);
+//             se.clear(); 
+//         }
+//         int mx = 0;
+//       for(int i = 0; i < v.size(); i++) {
+//         int t = v[i].size();
+//         mx = max(mx,t);
+//     }
+//         return mx;
+//     }
+// };
