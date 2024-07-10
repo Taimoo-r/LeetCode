b4c293cfb39b853b4df2e33ce4d5b4787de72f16
Taimoo-r <taimoorking23102004@gmail.com>
2024-07-10 10:51:48 +0500

    Time: 84 ms (42.40%) | Memory: 62 MB (82.32%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,14 +1,43 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        vector<int> v;
-        v = arr;
-        sort(v.begin(),v.end());
-        int peak = v.back();
-        map<int,int> mp;
-        for(int i = 0 ; i < arr.size(); i ++){
-            mp[arr[i]] = i;
+        int start=0,end=arr.size()-1;
+        int mid=(start+end)/2;
+        while(start<end){
+            if(arr[mid]<arr[mid+1]){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+            mid=(start+end)/2;
         }
-        return mp[peak];
+        return start;
+        // vector<int> v;
+        // v = arr;
+        // sort(v.begin(),v.end());
+        // int peak = v.back();
+
+        // int start = 0,end = v.size()-1; 
+        // while(start < end){
+        //     int mid = (start + end) / 2;
+        //     if(arr[mid]==peak)
+        //     return mid;
+        //     else if(arr[mid] < peak)
+        //     start = mid + 1;
+        //     else
+        //     end = mid-1; 
+        // }
+        // return 0;
+
+        //  vector<int> v;
+        // v = arr;
+        // sort(v.begin(),v.end());
+        // int peak = v.back();
+        // map<int,int> mp;
+        // for(int i = 0 ; i < arr.size(); i ++){
+        //     mp[arr[i]] = i;
+        // }
+        // return mp[peak];
     }
 };
