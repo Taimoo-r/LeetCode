c3f00c76bbbb4e0c4825e35abd68a3b473cf8467
Taimoo-r <taimoorking23102004@gmail.com>
2024-07-13 09:45:01 +0500

    Time: 25 ms (33.33%) | Memory: 13.6 MB (23.91%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,34 +1,58 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<int> st;
+    //     stack<int> st;
+    //     int n = s.size();
+        
+        
+    //     for (int i = 0; i < n; i++) {
+    //         if (s[i] == '(') {
+    //             st.push(i);
+    //         } else if (s[i] == ')') {
+    //             if (!st.empty() && s[st.top()] == '(') {
+    //                 st.pop();
+    //             } else {
+    //                 st.push(i);
+    //             }
+    //         }
+    //     }
+        
+    //     while (!st.empty()) {
+    //         s[st.top()] = '#'; 
+    //         st.pop();
+    //     }
+        
+    //     string res;
+    //     for (int i = 0; i < n; i++) {
+    //         if (s[i] != '#') {
+    //             res += s[i];
+    //         }
+    //     }
+        
+    //     return res;
+    // }
+    stack<int> st;
+        string res;
         int n = s.size();
         
-        
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') {
+        for(int i = 0 ; i < n; i++){
+            if(s[i]=='(')
+            st.push(i);
+            else if(s[i]==')'){
+                if(st.empty() || s[st.top()]==')')
                 st.push(i);
-            } else if (s[i] == ')') {
-                if (!st.empty() && s[st.top()] == '(') {
-                    st.pop();
-                } else {
-                    st.push(i);
-                }
+                else if(!st.empty() && s[st.top()]=='(')
+                st.pop();
             }
         }
-        
-        while (!st.empty()) {
-            s[st.top()] = '#'; 
-            st.pop();
-        }
-        
-        string res;
-        for (int i = 0; i < n; i++) {
-            if (s[i] != '#') {
-                res += s[i];
+        for(int i = n-1; i > -1; i--){
+            if(!st.empty() && i==st.top()){
+                st.pop();
+            }else{
+                res+=s[i];
             }
         }
-        
+        reverse(res.begin(),res.end());
         return res;
-    }
+    } 
 };
