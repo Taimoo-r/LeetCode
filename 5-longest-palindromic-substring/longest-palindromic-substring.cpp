d9cf6933b569caaf8ab4f1f16bd32c4158a43de4
Taimoo-r <taimoorking23102004@gmail.com>
2024-07-13 12:17:00 +0500

    Time: 13 ms (77.87%) | Memory: 8.5 MB (72.94%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int n = s.size();
+        int start = 0, maxLength = 1;
+
+        auto expandAroundCenter = [&](int left, int right) {
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            int len = right - left - 1;
+            if (len > maxLength) {
+                start = left + 1;
+                maxLength = len;
+            }
+        };
+
+        for (int i = 0; i < n; ++i) {
+            expandAroundCenter(i, i);       // Odd length palindromes
+            expandAroundCenter(i, i + 1);   // Even length palindromes
+        }
+
+        return s.substr(start, maxLength);
+    }
+};
+
