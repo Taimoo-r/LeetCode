3fccdc4808fe0cc2468e33d33358e396d7a5057a
Taimoo-r <taimoorking23102004@gmail.com>
2024-07-16 11:27:19 +0500

    Time: 2 ms (69.14%) | Memory: 10.2 MB (16.40%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
@@ -1,9 +1,36 @@
 class Solution {
 public:
-	string encode(string longUrl) {
-		return longUrl;
-	}
-	string decode(string shortUrl) {
-	   return shortUrl;   
-	}
+    unordered_map<string,string> mp;
+
+    string hashing(string longUrl){
+        string res = "";
+        for(int i = 0 ; i < 6 ; i++){
+        int random = rand() % 3;
+        if(random==1)
+        res+=('0' + random % 10);
+        else if(random==2)
+        res+=('a' + random % 26);
+        else
+        res+=('A' + random % 26);
+        }
+        mp[res] = longUrl;
+        return res;
+    }
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        string tinyUrl = "http://tinyurl.com/";
+        string hashed = hashing(longUrl);
+        return tinyUrl+=hashed;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return mp[shortUrl.substr(shortUrl.size()-6,6)];
+        
+    }
 };
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
