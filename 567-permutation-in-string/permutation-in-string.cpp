d222785cb3b85fd5978a65719a7a8e5b237eb3b7
Taimoo-r <taimoorking23102004@gmail.com>
2024-07-23 12:02:24 +0500

    Time: 4 ms (81.06%) | Memory: 8.9 MB (42.28%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.size() > s2.size()) return false;
-
-        vector<int> s1Freq(26, 0), s2Freq(26, 0);
-
-        // Initialize frequency array for s1
-        for (char c : s1) {
-            s1Freq[c - 'a']++;
-        }
-
-        // Initialize frequency array for the first window of s2
-        for (int i = 0; i < s1.size(); i++) {
-            s2Freq[s2[i] - 'a']++;
-        }
-
-        // Check if the first window is a permutation of s1
-        if (s1Freq == s2Freq) return true;
-
-        // Slide the window over s2
-        for (int i = s1.size(); i < s2.size(); i++) {
-            s2Freq[s2[i] - 'a']++;
-            s2Freq[s2[i - s1.size()] - 'a']--;
-            if (s1Freq == s2Freq) return true;
-        }
-
+        if(s1.size()>s2.size())
         return false;
+
+        vector<int> s1F(26,0), s2F(26,0);
+
+        for(int i = 0; i < s1.size(); i++){
+            s1F[s1[i] - 'a']++;
+        }
+        for(int i = 0 ; i < s1.size() ; i++){
+            s2F[s2[i] - 'a']++;
+        }
+        if(s1F==s2F)
+        return true;
+        for(int i = s1.size() ; i < s2.size(); i++){
+            s2F[s2[i]-'a']++;
+            s2F[s2[i - s1.size()] - 'a']--;
+            if(s1F==s2F)
+            return true;
+        }
+        return false;
+        
     }
 };
