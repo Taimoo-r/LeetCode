3c939fcb5a083e356446551ffff62d6220f345c7
Taimoo-r <taimoorking23102004@gmail.com>
2024-07-23 09:55:55 +0500

    Time: 25 ms (23.48%) | Memory: 16.6 MB (10.14%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int left,right,ans;
-        left = right = ans = 0;
-        int n = s.size();
-        set<char> S;
-        while(left < n && right < n){
-            if(S.find(s[right])==S.end()){
-                S.insert(s[right]);
-                right++;
-                ans = max(ans,right-left);
-                }
+        if(s.size()==0 || s.size()==1)
+        return s.size();
+        int longest = 0;
+        int i = 0, j = 0;
+        set<char> set;
+        while(j < s.size()){
+            if(!set.count(s[j])){
+                set.insert(s[j]);
+                j++;
+                longest = max(longest, j-i);
+            }
             else{
-                S.erase(s[left]);
-                left++;
+                set.erase(s[i]);
+                i++;
             }
         }
-        return ans;
+        return longest; 
+        
     }
 };
-//         unordered_set<char> se;
-//         vector<unordered_set<char>> v;
-//         int count = 0;
-//         int n = s.size();
-//         for(int i = 0 ; i < n ; i++){
-//             for(int j = i ; j < n && se.find(s[j])==se.end();j++){
-//                 se.insert(s[j]);
-//             }
-//             v.push_back(se);
-//             se.clear(); 
-//         }
-//         int mx = 0;
-//       for(int i = 0; i < v.size(); i++) {
-//         int t = v[i].size();
-//         mx = max(mx,t);
-//     }
-//         return mx;
-//     }
-// };
