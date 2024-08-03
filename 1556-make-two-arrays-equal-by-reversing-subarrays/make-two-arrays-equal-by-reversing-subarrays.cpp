d524290766ce582bf81fd2c7d54925366d9f7d9c
Taimoo-r <taimoorking23102004@gmail.com>
2024-08-03 10:26:10 +0500

    Time: 16 ms (10.57%) | Memory: 20.1 MB (14.13%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,14 +1,27 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        if(target==arr)
-            return true;
-        unordered_map<int,int> m1,m2;
-        for(int i = 0 ; i < target.size(); i++){
-            m1[target[i]]++;
-            m2[arr[i]]++;
+        if(target.size() != arr.size()) {
+            return false;
         }
-        return m1==m2 ? true : false;
         
+        unordered_map<int, int> countMap;
+        
+        for(int i = 0; i < target.size(); ++i) {
+            countMap[target[i]]++;
+            countMap[arr[i]]--;
+        }
+        
+        for(auto& count : countMap) {
+            if(count.second != 0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
