729a4d700b01f5bcaa9d6d7c3602fcef0be41a1d
Taimoo-r <taimoorking23102004@gmail.com>
2024-08-07 10:11:57 +0500

    Time: 11 ms (39.04%) | Memory: 16.2 MB (28.64%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        vector<string> ans(score.size());
-        int count = 0;
+        map<int,int> mp;
+        for(int i = 0 ; i < score.size() ; i++){
+            mp[score[i]] = i;
+        }
         priority_queue<int> pq(score.begin(),score.end());
-        for(int i = 0 ; i < score.size() && !pq.empty(); i++){
-            for(int j = 0 ; j < score.size(); j++){
-                if(pq.top()==score[j]){
-                    count++;
-                    if(count==1)
-                    ans[j] = "Gold Medal";
-                    else if(count==2)
-                    ans[j] = "Silver Medal";
-                    else if(count==3)
-                    ans[j] = "Bronze Medal";
-                    else
-                    ans[j] = to_string(count);
-                    pq.pop();
-                }
-            }
+        vector<string> ans(score.size());
+        int c = 0;
+        while(!pq.empty()){
+            int top = pq.top();
+            pq.pop();
+            c++;
+            if(c==1)
+            ans[mp[top]] = "Gold Medal";
+            else if(c==2)
+            ans[mp[top]] = "Silver Medal";
+            else if(c==3)
+            ans[mp[top]] = "Bronze Medal";
+            else
+            ans[mp[top]] = to_string(c);
         }
         return ans;
+
         
     }
 };
