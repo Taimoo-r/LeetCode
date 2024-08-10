b51c4ca15357116d44e6587ea342567dcd1a02bf
Taimoo-r <taimoorking23102004@gmail.com>
2024-08-10 13:36:46 +0500

    Time: 7 ms (34.42%) | Memory: 14.6 MB (18.70%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -15,23 +15,35 @@ public:
 
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
+        set<vector<int>> set;
         vector<int> comb;
-        helper(nums, target, 0, comb);
+        helper(nums, target, 0, 0, comb, set);
         return res;
     }
 
-    void helper(vector<int>& nums, int target, int index, vector<int>& comb) {
-        if (target == 0) {
-            res.push_back(comb);
+    void helper(vector<int>& nums, int target, int index, int sum, vector<int>& comb, set<vector<int>>& set) {
+        if (sum == target) {
+            if (set.find(comb) == set.end()) {
+                set.insert(comb);
+                res.push_back(comb);
+            }
             return;
         }
 
-        for (int i = index; i < nums.size() && nums[i] <= target; i++) {
-            if (i > index && nums[i] == nums[i - 1]) continue; // Skip duplicates
-
-            comb.push_back(nums[i]);
-            helper(nums, target - nums[i], i + 1, comb);  // i + 1 to move to the next index
-            comb.pop_back();  // Backtrack
+        if (sum > target || index >= nums.size()) {
+            return;
         }
+
+        // Inclusion
+        comb.push_back(nums[index]);
+        helper(nums, target, index + 1, sum + nums[index], comb, set);
+
+        // Exclusion
+        comb.pop_back();
+        while (index + 1 < nums.size() && nums[index] == nums[index + 1]) {
+            index++;
+        }
+
+        helper(nums, target, index + 1, sum, comb, set);
     }
 };
