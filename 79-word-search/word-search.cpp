1de841298dacb055f1e243372b3b75f7f852d4dd
Taimoo-r <taimoorking23102004@gmail.com>
2024-08-12 10:51:53 +0500

    Time: 1040 ms (18.55%) | Memory: 12.1 MB (19.95%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        bool ans ;
-        for(int row = 0 ; row < board.size(); row++){
-            for(int col = 0 ; col < board[row].size(); col++){
-                if(word[0] == board[row][col]){
-                if(helper(board, word , col, row, 0))
-                return true;
+        int rowSize = board.size();
+        int colSize = board[0].size();
+        for(int row = 0 ; row < rowSize; row++){
+            for(int col = 0 ; col < colSize ; col++){
+                if(word[0]==board[row][col]){
+                    if(helper(board, word, row, col, 0))
+                    return true;
                 }
             }
         }
         return false;
     }
-
-    bool helper(vector<vector<char>>& board, string &word, int col, int row, int index){
-        if(index >= word.size()){
-            return true;
-        }
-        if(row >= board.size() || board[row].size() <= col || row <0 || col < 0 || board[row][col]==' ' || board[row][col]!=word[index]){
-            return false;
-        }
-        board[row][col] = ' ';
-
-        if(helper(board, word, col, row+1, index + 1) || helper(board, word, col-1, row, index + 1) || helper(board, word, col+1, row, index + 1) || helper(board, word, col, row-1, index + 1) )
+    bool helper(vector<vector<char>>& board, string word, int row, int col, int index){
+        if(index == word.size())
         return true;
+        if(row >= board.size() || row < 0 || col >= board[0].size() || col < 0 || board[row][col]!=word[index])
+        return false;
+        if(board[row][col]=='#')
+        return false;
+        board[row][col]= '#';
+        if( helper(board, word, row - 1, col, index + 1)
+            || helper(board, word, row + 1, col, index + 1)
+            || helper(board, word, row, col - 1, index + 1)
+            || helper(board, word, row, col + 1, index + 1))
+            return true;
         board[row][col] = word[index];
         return false;
     }
