054d0b61ff14f3550889b90e4cfae7db60df9968
Taimoo-r <taimoorking23102004@gmail.com>
2024-08-14 13:01:56 +0500

    Time: 19 ms (72.84%) | Memory: 18.7 MB (58.53%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> arr[n+1];
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(i!=j && isConnected[i][j]==1){
-                    arr[i].push_back(j);
-                    arr[j].push_back(i);
-                }
-            }
-        }
-        for(int i = 0; i < n ; i++){
-            cout<<"Node "<<i+1<<" : ";
-            for(auto it : arr[i]){
-                cout<<it+1<<" ";
-            }
-            cout<<endl;
-        }
         int res = 0;
-        set<int> visit, visitDFS;
-        for(int i = 0 ; i < n ; i++){
-            if(!visit.count(i)){
+        set<int> visit;
+        for(int i = 0; i < isConnected.size(); i++) {  // Start from index 0
+            if(!visit.count(i)) {
                 res++;
-                dfs(arr, visit, i);
+                dfs(isConnected, visit, i);
             }
         }
         return res;
-        
     }
-    void dfs(vector<int> adj[], set<int>& visit, int source){
+
+    void dfs(vector<vector<int>>& isConnected, set<int>& visit, int source) {  // Pass by reference
         visit.insert(source);
-        for(auto it : adj[source]){
-            if(!visit.count(it) ){
-                dfs(adj, visit, it);
+        for(int i = 0; i < isConnected.size(); i++) {
+            if(isConnected[source][i] == 1 && !visit.count(i)) {  // Check connection and visit status
+                dfs(isConnected, visit, i);
             }
         }
-
-
     }
 };
