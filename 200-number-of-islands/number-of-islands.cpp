30c4f79b731db4d1a209cdc4a966de5d1f40c61b
Taimoo-r <taimoorking23102004@gmail.com>
2024-08-17 14:17:08 +0500

    Time: 26 ms (75.07%) | Memory: 16.1 MB (76.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,25 +3,24 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int cnt = 0;
+        int res = 0;
         for(int row = 0 ; row < m ; row++){
             for(int col = 0 ; col < n ; col++){
                 if(grid[row][col]=='1'){
-                    cnt++;
+                    res++;
                     dfs(grid, row, col);
                 }
             }
         }
-        return cnt;
+        return res;
     }
-    
     void dfs(vector<vector<char>>& grid, int row, int col){
-        if(row >= 0 && row < grid.size() && col >= 0 && col < grid[0].size() && grid[row][col]!='0')
-        grid[row][col] = '0';
-        else return;
+        if(row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() || grid[row][col]=='0')
+        return;
+        grid[row][col]='0';
         dfs(grid, row, col + 1);
         dfs(grid, row, col - 1);
-        dfs(grid, row+1, col);
-        dfs(grid, row-1, col);
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
     }
 };
