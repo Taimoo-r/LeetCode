0740e3325e1e214f9491e1f723a364c215f655c6
Taimoo-r <taimoorking23102004@gmail.com>
2024-09-16 11:43:55 +0500

    Time: 12 ms (59.05%) | Memory: 15.8 MB (9.78%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
-            }
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i = 0 ; i < n ; i++){
+            mp[nums[i]] = i;
         }
-        return {};
-}
+        for(int i = 0 ; i < n ;i++){
+            int find = target - nums[i];
+            if(mp.count(find) && mp[find]!=i)
+                return {i,mp[find]};
+        }
+        vector<int> ans;
+        return ans;
+    }
 };
