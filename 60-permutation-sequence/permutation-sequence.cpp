2d267e059f1ffa64e75f358f1249f2e590b1dd7f
Taimoo-r <taimoorking23102004@gmail.com>
2024-10-27 00:18:18 -0700

    Time: 127 ms (10.28%) | Memory: 7.5 MB (92.86%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,55 +1,54 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-    //     vector<int> v(n);
-    //     for(int i = 0 ; i < n ; i++){
-    //         v[i] = i+1;
-    //     }
-    //     sort(v.begin(), v.end());
-    //     int i = 0;
-    //     string ans = "";
-    // do {
-    //     for (int &num : v) {
-    //         if(i == k - 1){
-    //             ans+= '0'+num;
-    //         }
-    //     }
-    //     i++;
-    // } while (std::next_permutation(v.begin(), v.end()) && i < k);
-
-    // return ans;
-        vector<int> nums(n);
-        for (int i = 0; i < n; i++) {
-            nums[i] = i + 1;
+        vector<int> v(n);
+        for(int i = 0 ; i < n ; i++){
+            v[i] = i+1;
         }
-        
-        vector<string> res;
-        vector<int> visit(n, 0);
-        string temp = "";
-        helper(nums, visit, temp, res, k);
-        return res[k - 1];
-    }
-    
-    void helper(vector<int>& nums, vector<int>& visit, string& temp, vector<string>& res, int k) {
-        if (temp.size() == nums.size()) {
-            res.push_back(temp);
-            return;
-        }
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (!visit[i]) {
-                visit[i] = 1; // Mark as visited
-                temp += to_string(nums[i]); // Append current number as a string
-                
-                // Stop if we've collected enough permutations
-                if (res.size() < k) {
-                    helper(nums, visit, temp, res, k);
-                }
-                
-                // Backtrack
-                visit[i] = 0; // Unmark as visited
-                temp.pop_back(); // Remove last character from temp
+        sort(v.begin(), v.end());
+        int i = 0;
+        string ans = "";
+    do {
+        for (int &num : v) {
+            if(i == k - 1){
+                ans+= '0'+num;
             }
         }
+        i++;
+    } while (std::next_permutation(v.begin(), v.end()) && i < k);
+
+    return ans;
+
+        // vector<vector<int>> res;
+        // vector<int> temp;
+        // vector<int> nums(n);
+        // for(int i = 0 ; i < n ; i++){
+        //     nums[i] = i+1;
+        // }
+        // vector<int> visit(nums.size(), 0);
+        // for(int i = 0 ; i < nums.size() ; i++){
+        //     if(!visit[i]){
+        //         helper(nums, i, res, visit, temp, k);
+        //     }
+        // }
+        // string ans = "";
+        // for(int i = 0 ; i < n ; i++){
+        //     ans+='0'+res[k][i];
+        // }
+        // cout<<ans;
+        // return "";
+    }
+    void helper(vector<int>& nums, int i, vector<vector<int>> &res, vector<int> &visit, vector<int> &temp, int k){
+        visit[i] = 1;
+        temp.push_back(nums[i]);
+        if(res.size()==k) return;
+        if(temp.size()==nums.size())
+            res.push_back(temp);
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(!visit[i])
+                helper(nums, i, res, visit, temp,k);
+        }
+        visit[i] = 0;
+        temp.pop_back();
     }
 };
