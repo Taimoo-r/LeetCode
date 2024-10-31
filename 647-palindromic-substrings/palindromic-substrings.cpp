8347a70a95f7caa7a1323d5053e1c4f0fa675f31
Taimoo-r <taimoorking23102004@gmail.com>
2024-10-30 20:36:14 -0700

    Time: 7 ms (70.08%) | Memory: 7.8 MB (77.64%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,19 +1,45 @@
+// class Solution {
+// public:
+//     int countSubstrings(string s) {
+//         int n = s.size();
+//         int count = 0;
+//         for(int i = 0 ; i < n; i++){
+//             count++;
+//             string temp = "";
+//             temp+=s[i];
+//             for(int j = i + 1 ; j < n ; j++){
+//                 temp+=s[j];
+//                 string rev = temp;
+//                 reverse(rev.begin(), rev.end());
+//                 if(rev==temp) count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.size();
         int count = 0;
-        for(int i = 0 ; i < n; i++){
-            count++;
-            string temp = "";
-            temp+=s[i];
-            for(int j = i + 1 ; j < n ; j++){
-                temp+=s[j];
-                string rev = temp;
-                reverse(rev.begin(), rev.end());
-                if(rev==temp) count++;
+
+        // Lambda to count palindromes from a center
+        auto expandAroundCenter = [&](int left, int right) {
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                count++;
+                left--;
+                right++;
             }
+        };
+
+        for (int i = 0; i < n; ++i) {
+            // Odd-length palindromes (single character center)
+            expandAroundCenter(i, i);
+            // Even-length palindromes (two character center)
+            expandAroundCenter(i, i + 1);
         }
+
         return count;
     }
 };
