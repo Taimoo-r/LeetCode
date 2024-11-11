e0b8ec9c550e231ff6307d02117b60fa74d7ea4f
Taimoo-r <taimoorking23102004@gmail.com>
2024-11-11 07:47:06 -0800

    Time: 233 ms (24.10%) | Memory: 31.4 MB (28.37%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int i, j, k;
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
         vector<vector<int>> res;
-        for(int i = 0 ; i < n; i++){
-            if(i > 0 && nums[i]==nums[i-1])
-            continue;
-            j = i + 1;
-            k = n - 1;
+        set<tuple<int, int, int>> uniq;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i = 0 ; i < n - 2 ; i++){
+        int j = i+1, k = n - 1;
             while(j < k){
-                if(nums[i]+nums[j]+nums[k]==0){
-                    res.push_back({nums[i],nums[j],nums[k]});
-                    while(j < k && nums[j]==nums[j+1]) j++;
-                    while(j < k && nums[k]==nums[k-1]) k--;
-                    j++;
+                if(nums[i]+nums[j]+nums[k] > 0)
                     k--;
-                }
-                else if(nums[i]+nums[j]+nums[k]<0)
-                j++;
-                else
-                k--;
+                else if(nums[i]+nums[j]+nums[k] < 0)
+                    j++;
+                else{
+                    if(uniq.find({nums[i], nums[j], nums[k]})==uniq.end())
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    uniq.insert({nums[i], nums[j], nums[k]});
+                        k--;
+                        j++;
+                    }
             }
         }
-        return res; 
+        return res;
+       
     }
 };
