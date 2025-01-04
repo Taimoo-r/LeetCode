aa703c681148aa0b3df1f21cce6996fb0035fc12
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-01-04 12:51:32 +0500

    Time: 3 ms (100.00%) | Memory: 54.3 MB (75.55%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums){
-        set<int> s(nums.begin(),nums.end());
-        int longest = 0;
-        for(const int& n : s){
+        // set<int> s(nums.begin(),nums.end());
+        // int longest = 0;
+        // for(const int& n : s){
             
-            if(s.find(n - 1)==s.end()){
-                int current = n;
-                int currLong = 1;
-                while(s.find(current + 1)!=s.end()){
-                    current+=1;
-                    currLong+=1;
-                }
-                longest = max(longest,currLong);
-            }
-        }
-        return longest;
-
-        
-        // int n = nums.size();
-        // if(n < 1) return 0;
-        // int consCnt = 1;
-        // int maxCnt = 0;
-        // sort(nums.begin(), nums.end());
-        // for(int i = 0 ; i < n - 1 ; i++){
-        //     if(nums[i+1]==nums[i]) continue;
-
-        //     if(nums[i+1]-nums[i]==1) consCnt++;
-
-        //     else if(nums[i+1] - nums[i]!=1){
-        //         maxCnt = max(maxCnt, consCnt);
-        //         consCnt = 1;
+        //     if(s.find(n - 1)==s.end()){
+        //         int current = n;
+        //         int currLong = 1;
+        //         while(s.find(current + 1)!=s.end()){
+        //             current+=1;
+        //             currLong+=1;
+        //         }
+        //         longest = max(longest,currLong);
         //     }
         // }
-        // maxCnt = max(maxCnt, consCnt);
-        // return maxCnt;
+        // return longest;
+
+        
+        int n = nums.size();
+        if(n < 1) return 0;
+        int consCnt = 1;
+        int maxCnt = 0;
+        sort(nums.begin(), nums.end());
+        for(int i = 0 ; i < n - 1 ; i++){
+            if(nums[i+1]==nums[i]) continue;
+
+            if(nums[i+1]-nums[i]==1) consCnt++;
+
+            else if(nums[i+1] - nums[i]!=1){
+                maxCnt = max(maxCnt, consCnt);
+                consCnt = 1;
+            }
+        }
+        maxCnt = max(maxCnt, consCnt);
+        return maxCnt;
     }
     
 };
