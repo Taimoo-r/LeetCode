6a527749f70be07d0ba67bf745636e7d4e22b256
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-01-13 10:04:04 +0500

    Time: 0 ms (100.00%) | Memory: 15.1 MB (76.59%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int search(vector<int>& a, int target) {
-        int s = 0, e = a.size()-1, ans = INT_MAX;
-        int n = a.size();
-        while(s <= e){
-            int m = s + (e - s)/2;
-            if(a[m]==target)
-            return m;
-            // if right half is sorted
-            if(a[m] <= a[e]){
-                if(target > a[m] && target <= a[e])
-                s = m + 1;
-                else 
-                e = m-1;
-            }
-            // if left half is sorted
-            else{
-                if(target < a[m] && target >= a[s])
-                e = m - 1;
-                else 
-                s = m+1;
-            }
+    int search(vector<int>& nums, int t) {
+        int n = nums.size();
+        int l = 0, h = n - 1;
+        while(l <= h){
+            int m = l + (h - l)/2;
 
+            if(nums[m] == t) return m;
+
+            else if(nums[m] >= nums[l]){  // kia ye left sorted hai?
+                if(nums[l] <= t && nums[m] >= t) h = m - 1;
+                else l = m + 1;
+            }
+            else if(nums[m] <= nums[h]){
+                if(nums[h] >= t && nums[m] <= t) l = m + 1;
+                else h = m - 1;
+            }
         }
         return -1;
-        
     }
 };
