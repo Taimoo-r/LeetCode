375e8304693eef6ed651f7cbd6972fdc9f9d52c5
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-01-13 09:58:10 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (16.70%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int t) {
-        for(auto num : nums) {
-            if(num==t) return true;
+        int n = nums.size();
+        int l = 0, h = n - 1;
+        while(l <= h){
+            int m = l + (h - l)/2;
+            if(t==nums[m]) return true;
+            else if(nums[l]==nums[m] && nums[m]==nums[h]){
+                l++;
+                h--;
+            }
+            else if(nums[l] <= nums[m]){ // ye wala left sara sorted hai
+                if(nums[l] <= t && nums[m] >= t) // aab ye check kro kay kia t isme hai?
+                    h = m - 1; // agr hai to just hum left-to-mid tk apni search krden ge
+                else    // agr nhi hai
+                    l = m + 1; // means wo phr mid-to-right mein hai
+            }
+            else if(nums[h] >= nums[m] ){
+                if(nums[h] >= t && nums[m] <= t)
+                    l = m + 1;
+                else
+                    h = m - 1;
+            }
         }
         return false;
-        
     }
 };
