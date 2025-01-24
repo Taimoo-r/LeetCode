d989a304aabdd9b6c6ca2d624c3a37ec4de042ba
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-01-24 15:25:21 +0500

    Time: 47 ms (59.95%) | Memory: 57.5 MB (60.72%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    bool isPal(string s){
-        int l = 0, r = s.size() - 1;
-        while(l < r){
-            if(s[l++]!=s[r--]) return 0;
+    bool isPal(string s, int st, int en){
+        while(st < en){
+            if(s[st++]!=s[en--]) return 0;
         }
-        cout<<s<<" "<<endl;
         return 1;
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        string curr = "";
         vector<string> v;
         gen(s, ans, v, 0);
         return ans;
@@ -21,7 +18,7 @@ public:
             return;
         }
         for(int i = idx ; i < s.size() ; i++){
-            if( isPal(s.substr(idx, i-idx+1))){
+            if( isPal(s, idx, i)){
                 v.push_back(s.substr(idx, i-idx+1));
                 gen(s, ans, v, i+1);
                 v.pop_back();
