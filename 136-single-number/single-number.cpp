35af0383ab96f38a641a66fbd1845d722da4f675
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-01-31 12:02:55 +0500

    Time: 6 ms (23.23%) | Memory: 20.5 MB (98.18%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -4,11 +4,10 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
         if(n==1) return nums[0];
-        for(int i = 0 ; i < nums.size() ; i+=2){
-            if(i < n){
-                if(nums[i]!=nums[(i+1)%n])
-                    return nums[i];
-            }
+        for(int i =0 ; i < nums.size(); i++){
+            if(i-1 >=0 && i+1 < n && (nums[i]^nums[i+1])!=0 && (nums[i]^nums[i-1])!=0) return nums[i]; 
+            if(i-1 < 0 && (nums[i]^nums[i+1])!=0) return nums[i];
+            if(i+1 >= n && (nums[i]^nums[i-1])!=0) return nums[i];
         }
         return 0;
     }
