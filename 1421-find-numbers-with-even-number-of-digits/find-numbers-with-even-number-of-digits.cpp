4fddefed9938000435a50f20816a5a6eb5c54f03
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-02-01 13:18:26 +0500

    Time: 0 ms (100.00%) | Memory: 13.4 MB (43.50%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -3,8 +3,8 @@ public:
     int findNumbers(vector<int>& nums) {
         int cnt = 0;
         for(auto &it : nums){
-            string s = to_string(it);
-            (s.size()&1)==0 ? cnt++ : cnt;
+            if((it >= 10 && it <= 99 )|| (it > 999 && it<=9999) || (it > 99999 && it <= 999999))
+                cnt++;
         }
         return cnt;
     }
