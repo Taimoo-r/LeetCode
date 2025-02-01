9b42e126531c31a5f3ebc7e9850cfe221e6085b8
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-02-01 15:54:37 +0500

    Time: 1767 ms (5.15%) | Memory: 11.5 MB (68.95%) - LeetSync

---
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSplits(string s) {
+        int n = s.size();
+        map<char, int> sl, sr;
+        sl[s[0]]++;
+        for(int i = 1 ; i < n ; i++) sr[s[i]]++;
+        int cnt = 0;
+        int l = 0, r = 1;
+        while(l < r && r < n){
+
+            cnt+=sl.size() == sr.size();
+            sl[s[r]]++;
+            sr[s[r]]--;
+            if(sr[s[r]]==0) sr.erase(s[r]);
+            cout<<"After : "<<sl.size()<<"=="<<sr.size()<<endl;
+            l++;
+            if(r + 1 < n) r++;
+        }
+        return cnt;
+        
+    }
+};
