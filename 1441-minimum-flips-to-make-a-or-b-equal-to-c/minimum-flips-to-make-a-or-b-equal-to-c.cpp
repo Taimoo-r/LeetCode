6bd6ace997b029dc9e2d3ec3369ef4847d60296d
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-02-02 13:42:56 +0500

    Time: 0 ms (100.00%) | Memory: 7.7 MB (67.84%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    string DtoB(int x){
-        string ans = "";
-        while(x > 0){
-            ans+=x%2+'0';
-            x/=2;
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
+    
     int minFlips(int a, int b, int c) {
         int checker = (a | b) ^ c;
         int cnt = 0;
-        cout<<DtoB(checker)<<endl;
         for(int i = 0 ; i < 32 ; i++){
             if((checker&(1<<i))!=0){
                 int whatWeNeed = c&(1<<i);
