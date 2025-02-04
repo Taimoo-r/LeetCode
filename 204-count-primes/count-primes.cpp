ccc3135015aac8d7d7c637924e76216732bb4395
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-02-04 11:20:23 +0500

    Time: 211 ms (31.65%) | Memory: 12.6 MB (91.17%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n){
+        vector<bool> v(n, true);
+
+        for(int i = 2 ; i <= sqrt(n) ; i++){
+            if(v[i]){
+                for(int j = i*i ; j<n ; j+=i){
+                    v[j] = false;
+                }
+            }
+        }
+        
+        int cnt = 0;
+        for(int i = 2 ; i < n ; i++){
+            if(v[i]) cnt++;
+        }
+        return cnt;
+
+        
+    }
+};
