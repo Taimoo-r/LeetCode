db0096083be0fb925c467918d6e9fb08a75656ed
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-02-05 12:38:48 +0500

    Time: 2 ms (13.49%) | Memory: 10.1 MB (5.37%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> S;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='{' || s[i]=='[' || s[i]=='(')
-            S.push(s[i]);
-            else{
-                if(S.empty())
-                return false;
-                else if(S.top()=='{' && s[i]=='}' || S.top()=='[' && s[i]==']' || S.top()=='(' && s[i]==')')
-                S.pop();
-                else 
-                return false;
+        stack<int> st;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                st.push(s[i]);
+            else if(!st.empty()){
+                if((s[i]==')' && st.top()=='(') or (s[i]==']' && st.top()=='[') or (s[i]=='}'
+                && st.top()=='{'))
+                    st.pop();
+                else return false;
             }
+            else
+                return false;
         }
-        if(S.empty())
-        return true;
-        else 
-        return false;
+        return st.empty() ? true : false;
     }
 };
