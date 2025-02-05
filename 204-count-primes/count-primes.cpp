51b77a6412841712f164cf52eb368368c3b4bf59
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-02-05 10:15:19 +0500

    Time: 155 ms (87.27%) | Memory: 132.5 MB (15.09%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int countPrimes(int n){
-        vector<bool> v(n, true);
+    int countPrimes(int n) {
+        // first we do pre-computations on array to find all Prime factors from [1->n) in O(1) time
+        // means we use Sieve of Eratosthenes by looping array of n size, & by flaging all multiples of  prime number to false, & always start from the square of that number to do that.
+
+        vector<int> v(n, 1);
 
         for(int i = 2 ; i <= sqrt(n) ; i++){
             if(v[i]){
-                for(int j = i*i ; j<n ; j+=i){
-                    v[j] = false;
+                for(int j = i*i ; j < n ; j+=i){
+                    v[j] = 0;
                 }
             }
         }
-        
         int cnt = 0;
         for(int i = 2 ; i < n ; i++){
             if(v[i]) cnt++;
