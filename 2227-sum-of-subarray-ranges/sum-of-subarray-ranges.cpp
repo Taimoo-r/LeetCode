cb1c79ada015dc6dc669c63699a17b114bda7654
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-02-14 11:08:05 +0500

    Time: 4 ms (63.78%) | Memory: 15.8 MB (43.13%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,18 +1,59 @@
 class Solution {
+    long long mini, maxi;
 public:
-    long long subArrayRanges(vector<int>& nums) {
+    void Minimums(vector<int> &nums){
         int n = nums.size();
-        long long maxi, mini;
-        long long sum = 0;
+        vector<int> left(n), right(n);
+        stack<int> st;
+
         for(int i = 0 ; i < n ; i++){
-            maxi = nums[i], mini = nums[i];
-            for(int j = i ; j < n ; j++){
-                maxi = max(maxi, (long long)nums[j]);
-                mini = min(mini, (long long)nums[j]);
-                sum+= maxi - mini;
-            }
+            while(!st.empty() && nums[st.top()] > nums[i]) st.pop();
+
+            left[i] = st.empty() ? i+1 : i - st.top();
+            st.push(i);
         }
-        return sum;
+
+        while(!st.empty()) st.pop();
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            while(!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            right[i] = st.empty() ? n - i :st.top() - i; 
+            st.push(i);
+        }
+        for(int i = 0 ; i < n ; i++){
+            mini+=left[i]*right[i]*(long long)nums[i]; 
+        }
+
+    }    
+    void Maximums(vector<int> &nums){
+        int n = nums.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && nums[st.top()] < nums[i]) st.pop();
+
+            left[i] = st.empty() ? i+1 : i - st.top();
+            st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]) st.pop();
+            right[i] = st.empty() ? n - i :st.top() - i; 
+            st.push(i);
+        }
+        for(int i = 0 ; i < n ; i++){
+            maxi+=left[i]*right[i]*(long long)nums[i]; 
+        }
+
+    }
+    long long subArrayRanges(vector<int>& nums) {
+        maxi = 0, mini = 0;
+        Maximums(nums);
+        Minimums(nums);
+        return maxi - mini;
         
     }
 };
