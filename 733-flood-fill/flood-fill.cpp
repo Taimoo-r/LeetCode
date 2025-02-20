7f36f6ec902fdf63c0f2d073f036f39e3abd3791
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-02-20 08:35:47 +0500

    Time: 0 ms (100.00%) | Memory: 18.3 MB (48.56%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int initialColor = image[sr][sc];
-        if(color!=initialColor)
-        dfs(image, sr, sc, color, initialColor);
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        fill(image, sr, sc, color, vis, image[sr][sc]);
         return image;
     }
-    
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int inC){
-        if(sr < 0 || sr >= image.size() || sc < 0 && sc >= image[0].size() || image[sr][sc]!=inC)
-        return;
-        image[sr][sc] = color;
-        dfs(image, sr, sc + 1, color , inC);
-        dfs(image, sr, sc - 1, color , inC);
-        dfs(image, sr + 1, sc, color , inC);
-        dfs(image, sr - 1, sc, color , inC);
+    void fill(vector<vector<int>>& image, int sr, int sc, int color, vector<vector<int>>& vis, int currCol){
+        if(vis[sr][sc] || image[sr][sc]!=currCol) return;
+        vis[sr][sc] = 1;
 
+        image[sr][sc] = color;
+        
+        if(sr+1 < image.size() && image[sr+1][sc] == currCol) fill(image, sr+1, sc, color, vis, currCol);
+        if(sr-1 >= 0 && image[sr-1][sc] == currCol) fill(image, sr-1, sc, color, vis, currCol); 
+        if(sc+1 < image[0].size() && image[sr][sc+1] == currCol) fill(image, sr, sc+1, color, vis, currCol);
+        if(sc-1 >= 0 && image[sr][sc-1] == currCol) fill(image, sr, sc-1, color, vis, currCol);
     }
 };
