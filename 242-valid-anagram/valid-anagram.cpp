00664d2ea5c825ec666ac2379c38f423949f2048
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-06-01 14:02:17 +0500

    Time: 0 ms (100.00%) | Memory: 9.7 MB (48.19%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return 0;
-        map<char, int> a, b;
+        unordered_map<char, int> a, b;
 
         for(int i = 0 ; i < s.size() ; i++){
             a[s[i]]++;
