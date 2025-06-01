622c8f2c32cbee6ddc25507224a85a1c3ca71008
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-06-01 12:50:24 +0500

    Time: 8 ms (16.46%) | Memory: 23.8 MB (10.08%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> words;
         string word = "";
         string ans = "";
         for(auto &it : s){
             if(it == ' ' && word.size() > 0){
-                words.push_back(word);
                 ans = word + " " + ans;
                 word = "";
             } 
             else if(it != ' ') word+=it;
         }
         if(word.size() > 0 && word[0]!=' ') {
-            words.push_back(word); 
             ans = word + " " + ans;
         }
 
