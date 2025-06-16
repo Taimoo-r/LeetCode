f19672fc3e5165cc98cf5f996b8af40436bf919a
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-06-16 13:23:18 +0500

    Time: 65 ms (22.41%) | Memory: 93 MB (14.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> mp;
-        for(auto &it : nums){
-            if(mp.count(it)) return true;
-            mp.insert(it);
-        }
-        return false;
-        
+        unordered_set<int> seen(nums.begin(), nums.end());
+        return seen.size() != nums.size();
     }
 };
