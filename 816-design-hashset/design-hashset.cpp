1115e89621777e88a1957d8acb44a4393b2df36e
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-06-16 15:05:01 +0500

    Time: 19 ms (51.89%) | Memory: 50.3 MB (43.74%) - LeetSync

--- 816-design-hashset/design-hashset.cpp
+++ 816-design-hashset/design-hashset.cpp
@@ -1,23 +1,23 @@
 class MyHashSet {
 public:
-    vector<int> v;
+    vector<bool> v;
+
     MyHashSet() {
-        v.resize(1e6+1, -1);
+        v = vector<bool>(1000001, false); // Step 1: Initialization
     }
-    
+
     void add(int key) {
-        v[key] = key;
+        v[key] = true; // Step 2: Mark presence
     }
-    
+
     void remove(int key) {
-        v[key] = -1;
+        v[key] = false; // Step 3: Mark absence
     }
-    
+
     bool contains(int key) {
-        return v[key] != -1;
+        return v[key]; // Step 4: Check presence
     }
 };
-
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet* obj = new MyHashSet();
