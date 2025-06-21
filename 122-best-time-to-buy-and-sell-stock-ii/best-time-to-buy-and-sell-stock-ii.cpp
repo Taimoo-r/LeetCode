821ee5d14062e56db1b223c1ad1909a25fc92844
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-06-21 11:52:55 +0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (67.24%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n = prices.size();
         int sum = 0;
         int low = prices[0];
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 1 ; i < n ; i++){
             if(low <= prices[i]){
                 sum+=prices[i]-low;
                 low = prices[i];
