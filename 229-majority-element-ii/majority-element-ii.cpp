5e3ed709a938d2aaf192556162561d7db9ccc27b
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-06-21 11:59:33 +0500

    Time: 0 ms (100.00%) | Memory: 23.5 MB (68.89%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int more = nums.size()/3;
-        set<int> st;
-        map<int, int> mp;
-        vector<int> v;
-        for(auto &it : nums) {
-            mp[it]++;
-            if(mp[it] > more && !st.count(it)) {
-                v.push_back(it);
-                st.insert(it);
+        int c1 = 0, c2 = 0;
+        int a = INT_MIN, b = INT_MIN;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(c1 == 0 && nums[i] != b) {
+                c1 = 1;
+                a = nums[i]; 
+            }
+            else if(c2 == 0 && nums[i] != a) {
+                c2 = 1;
+                b = nums[i];
+            }
+            else if(a == nums[i]) c1++;
+            else if(b == nums[i]) c2++;
+            else {
+                c1--;
+                c2--;
             }
         }
-
-        return v;
+        c1 = 0, c2 = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == a) c1++;
+            else if(nums[i] == b) c2++;
+        }
+        vector<int> ans;
+        int mini = (int)(n/3) + 1;
+        if(c1 >= mini) ans.push_back(a);
+        if(c2 >= mini) ans.push_back(b);
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
