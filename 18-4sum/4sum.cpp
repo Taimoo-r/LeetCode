f2a21e0b90431280c5244b60bccb3b47443c9c0e
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-06-25 16:13:59 +0500

    Time: 201 ms (10.15%) | Memory: 53.6 MB (11.88%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,61 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int t) {
-//         int ll, rr;
-//         ll = 0, rr = nums.size() - 1;
-//         sort(nums.begin(), nums.end());
-//         set<vector<int>> st;
-//         while(ll < rr){
-//             int l = ll + 1, r = rr - 1;
-//             while(l < r){
-//                 long long sum = nums[ll]+nums[rr]+nums[l]+nums[r];
-//                 if(sum < t){
-//                     if(ll - l < 0)
-//                         l++;
-//                     else
-//                         ll++;
-//                 }
-//                 else if(sum > t){
-//                     if(rr - r < 2)
-//                         r--;
-//                     else
-//                         rr--;
-//                 }
-//                 else if(sum == t){
-//                     // push logic
-//                     if(!st.count({nums[ll],nums[rr],nums[l],nums[r]})){
-//                         ans.push_back({nums[ll],nums[rr],nums[l],nums[r]});
-//                         st.insert({nums[ll],nums[rr],nums[l],nums[r]});
-//                     }
-//                     l++, r--;
-//                 }
-//             } 
-//                 ll++, rr--;
-//         }
-//         return ans;
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> ans;
+        set<vector<int>> st;
         sort(nums.begin(), nums.end());
-        for(int ll = 0 ; ll < nums.size() ; ll++){
-            if(ll > 0 && nums[ll]==nums[ll - 1]) continue;
+        int n = nums.size();
 
-            for(int rr = ll + 1 ; rr < nums.size(); rr++){
-                if(rr > ll + 1 && nums[rr]==nums[rr - 1]) continue;
-                int l = rr + 1, r = nums.size() - 1;
-                while(l < r){
-                    long sum = ( long)nums[ll]+( long)nums[rr]+( long)nums[l]+( long)nums[r];
-                    if(sum == t){
-                        ans.push_back({nums[ll],nums[rr],nums[l],nums[r]});
-                        while(l < r && nums[l] == nums[l+1]) l++;
-                        while(r > l && nums[r] == nums[r-1]) r--;
-                        l++, r--;
+        for (int i = 0; i < n - 3; ++i) {
+            for (int j = i + 1; j < n - 2; ++j) {
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum == target) {
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        if (!st.count(temp)) {
+                            st.insert(temp);
+                            ans.push_back(temp);
+                        }
+                        k++, l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
                     }
-                    else if(sum > t)
-                        r--;
-                    else
-                        l++;
                 }
             }
-        }  
-            return ans;
+        }
+
+        return ans;
     }
-};                             
+};
