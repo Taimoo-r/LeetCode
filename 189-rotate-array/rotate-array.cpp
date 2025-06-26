bd522a7324fa250d8732683f97ab33535c87334a
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-06-26 12:44:39 +0500

    Time: 0 ms (100.00%) | Memory: 29.6 MB (37.05%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,14 +1,25 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         k%=nums.size();
+//         vector<int> ans;
+//         int n = nums.size();
+//         for(int i = n - k ; i < n; i++){
+//             ans.push_back(nums[i]);
+//         }
+
+//         ans.insert(ans.end(), nums.begin(), nums.end() - k);
+//         nums = ans;
+//     }
+// };
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k%=nums.size();
-        vector<int> ans;
         int n = nums.size();
-        for(int i = n - k ; i < n; i++){
-            ans.push_back(nums[i]);
-        }
-
-        ans.insert(ans.end(), nums.begin(), nums.end() - k);
-        nums = ans;
+        k%=n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
