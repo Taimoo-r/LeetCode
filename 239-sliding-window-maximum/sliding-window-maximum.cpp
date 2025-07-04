ea7411331eb24c4786b8b7cd9c1f08507cf194e5
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-07-04 12:55:08 +0500

    Time: 69 ms (15.99%) | Memory: 153.2 MB (18.16%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> ans;
+        int maxi = INT_MIN;
         int n = nums.size();
+        vector<int> ans;
+        priority_queue<pair<int, int>> pq; 
+        for(int i = 0 ; i < n ; i++) {
 
-        // pair -> {num, index}
-        priority_queue<pair<int, int>> pq;
-
-        for (int i = 0; i < n; i++) {
             pq.push({nums[i], i});
 
-            // Only start recording answers after window is full
-            if (i >= k - 1) {
-                // Remove elements that are outside the window
-                while (pq.top().second <= i - k) {
-                    pq.pop();
-                }
+            if(i >= k - 1){
+            while(pq.top().second <= i - k) pq.pop();
 
-                // Top is max of current window
-                ans.push_back(pq.top().first);
+            ans.push_back(pq.top().first);
             }
         }
-
         return ans;
     }
 };
