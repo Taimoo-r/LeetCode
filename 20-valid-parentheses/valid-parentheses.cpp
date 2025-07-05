c422116e05685db249d6e20a507153b206307a72
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-07-05 12:28:58 +0500

    Time: 125 ms (1.59%) | Memory: 8.6 MB (83.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,15 +1,32 @@
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> st;
+//         for(auto &it : s){
+//             if(it == '(' || it == '{' || it == '['){
+//                 st.push(it);
+//             }
+//             else if(!st.empty() && ((st.top() == '(' && it == ')') || (st.top() == '{' && it == '}') ||
+//             (st.top() == '[' && it == ']'))) st.pop();
+//             else return false;
+//         }
+//         return st.empty();
+//     }
+// };
+
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(auto &it : s){
-            if(it == '(' || it == '{' || it == '['){
-                st.push(it);
-            }
-            else if(!st.empty() && ((st.top() == '(' && it == ')') || (st.top() == '{' && it == '}') ||
-            (st.top() == '[' && it == ']'))) st.pop();
-            else return false;
+        while (true) {
+            size_t lenBefore = s.size();
+            size_t pos;
+            while ((pos = s.find("()")) != string::npos) s.erase(pos, 2);
+            while ((pos = s.find("[]")) != string::npos) s.erase(pos, 2);
+            while ((pos = s.find("{}")) != string::npos) s.erase(pos, 2);
+
+            if (s.size() == lenBefore) break;  
         }
-        return st.empty();
+
+        return s.empty(); 
     }
 };
