4dc21e07ebc982bd6dbb2cb22520f97f1df8026c
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-07-05 12:29:43 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,32 +1,32 @@
-// class Solution {
-// public:
-//     bool isValid(string s) {
-//         stack<char> st;
-//         for(auto &it : s){
-//             if(it == '(' || it == '{' || it == '['){
-//                 st.push(it);
-//             }
-//             else if(!st.empty() && ((st.top() == '(' && it == ')') || (st.top() == '{' && it == '}') ||
-//             (st.top() == '[' && it == ']'))) st.pop();
-//             else return false;
-//         }
-//         return st.empty();
-//     }
-// };
-
 class Solution {
 public:
     bool isValid(string s) {
-        while (true) {
-            size_t lenBefore = s.size();
-            size_t pos;
-            while ((pos = s.find("()")) != string::npos) s.erase(pos, 2);
-            while ((pos = s.find("[]")) != string::npos) s.erase(pos, 2);
-            while ((pos = s.find("{}")) != string::npos) s.erase(pos, 2);
-
-            if (s.size() == lenBefore) break;  
+        stack<char> st;
+        for(auto &it : s){
+            if(it == '(' || it == '{' || it == '['){
+                st.push(it);
+            }
+            else if(!st.empty() && ((st.top() == '(' && it == ')') || (st.top() == '{' && it == '}') ||
+            (st.top() == '[' && it == ']'))) st.pop();
+            else return false;
         }
-
-        return s.empty(); 
+        return st.empty();
     }
 };
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         while (true) {
+//             size_t lenBefore = s.size();
+//             size_t pos;
+//             while ((pos = s.find("()")) != string::npos) s.erase(pos, 2);
+//             while ((pos = s.find("[]")) != string::npos) s.erase(pos, 2);
+//             while ((pos = s.find("{}")) != string::npos) s.erase(pos, 2);
+
+//             if (s.size() == lenBefore) break;  
+//         }
+
+//         return s.empty(); 
+//     }
+// };
