91c601137e62677b7ebb4ad16d2e16a68dd228f7
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-07-09 15:37:09 +0500

    Time: 49 ms (50.94%) | Memory: 84.5 MB (57.03%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& pos, vector<int>& speed) {
-        priority_queue<vector<double>> pq;
-        int fleet = 0;
-             for (int i = 0; i < pos.size(); i++) {
-             double time = (double)(target - pos[i]) / speed[i];
-             pq.push({(double)pos[i], (double)speed[i], time});
-        }
-        while(true){
-            if(pq.size()==1){
-                return ++fleet;
+       vector<pair<int, double>> v(pos.size());
+       int n = speed.size();
+       for(int i = 0 ; i < n ; i++){
+        v[i] = {pos[i], (double)(target - pos[i])/speed[i]};
+ 
+       }
+
+
+       sort(v.rbegin(), v.rend());
+       
+      stack<double> st;
+
+       for(auto &[__, time] : v){
+         if (st.empty() || time > st.top()) {
+                st.push(time);
             }
-            auto ahead = pq.top(); pq.pop();
-            auto behind = pq.top(); pq.pop();
-            if(ahead[2]>=behind[2]){
-                pq.push(ahead);
-            }
-            else if(ahead[2] <= behind[2]){
-                fleet++;
-                pq.push(behind);
-            }
-        }
     }
+        return st.size();
+    }
+    
+
 };
