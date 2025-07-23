7ecb7fcca8183526ad5db122d4da64c1717b83c3
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-07-23 15:00:12 +0500

    Time: 15 ms (22.02%) | Memory: 7.7 MB (64.03%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int Fibo(int x){
-        if(x==0)
-        return 0;
-        else if(x==1)
-        return 1;
-        return (Fibo(x-1)+Fibo(x-2));
+    int f(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return f(n - 1) + f(n - 2);
+
     }
     int fib(int n) {
-        return Fibo(n);
+        return f(n);
     }
 };
