ed45cbaa4ea3ea83d6e062d88f9ff2e833116866
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-07-23 13:25:17 +0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.31%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,24 +2,11 @@ class Solution {
 public:
     double calc(double x, long n){
         if(n==0) return 1;
-        double half=calc(x, n/2);
-        if(n%2==0) return half*half;
-        else return half*half*x;
+        if(n < 0) return calc(1/x, -n);
+        if(n%2==0) return calc(x*x, n/2);
+        else return x*calc(x*x, (n - 1)/2);
     }
     double myPow(double x, int n) {
-        if(n==0) return 1.0;
-        long nn = n;
-        if(n < 0)
-            nn = -nn;
-        else nn = n;
-        double ans = calc(x, nn/2);
-        if(n%2==0){
-            ans*=ans;
-        } else{
-            ans = ans*ans*x;
-        }
-        if(n < 0) ans = double(1.0)/ans;
-
-        return ans;
+        return calc(x, n);
     }
 };
