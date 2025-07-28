0d78fca468ddec80d9d7d16c7ed844475abe9dd3
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-07-28 12:20:31 +0500

    Time: 1 ms (76.35%) | Memory: 65 MB (90.07%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    bool search(int i, vector<int>& nums){
-        int st = 0, end = nums.size()-1;
-        while(st <= end){
-            int md = st + (end - st)/2;
-            if(md!=i && nums[md]==nums[i]){
-                return true;
-            }
-            else if(nums[md] > nums[i]) end = md - 1;
-            else st = md + 1;
-        }
-        return false;
+    // bool search(int i, vector<int>& nums){
+    //     int st = 0, end = nums.size()-1;
+    //     while(st <= end){
+    //         int md = st + (end - st)/2;
+    //         if(md!=i && nums[md]==nums[i]){
+    //             return true;
+    //         }
+    //         else if(nums[md] > nums[i]) end = md - 1;
+    //         else st = md + 1;
+    //     }
+    //     return false;
 
-    }
+    // }
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(search(i, nums)) return nums[i];
+        for(int i  = 0 ; i < nums.size(); i++){
+            if(nums[abs(nums[i])-1] < 0)
+                return abs(nums[i]);
+            else
+                nums[abs(nums[i])-1]*=-1;
         }
         return 0;
     }
