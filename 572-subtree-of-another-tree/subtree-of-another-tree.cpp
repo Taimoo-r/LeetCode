7efc2ccdaf5861c3192f0b6f3a66528a50548e55
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-07-31 12:35:36 +0500

    Time: 2 ms (32.26%) | Memory: 29 MB (71.22%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -11,15 +11,19 @@
  */
 class Solution {
 public:
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(!root) return false;
-        if(sameTree(root,subRoot)) return true;
-        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-    }
-    bool sameTree(TreeNode* root, TreeNode* subRoot){
+    bool same = false;
+    bool isSame(TreeNode* root, TreeNode* subRoot){
         if(!root && !subRoot) return true;
         if(!root || !subRoot) return false;
         if(root->val != subRoot->val) return false;
-        return sameTree(root->left,subRoot->left) && sameTree(root->right,subRoot->right);
+        bool left = isSame(root->left, subRoot->left);
+        bool right = isSame(root->right, subRoot->right);
+        same = left && right;
+        return same;
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root) return false;
+        if(isSame(root, subRoot)) return true;
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
