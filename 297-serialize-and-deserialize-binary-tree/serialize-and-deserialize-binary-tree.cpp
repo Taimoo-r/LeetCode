f57e3c617559a2eb519cb2381c709f35b941bc59
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-08-08 21:02:58 +0500

    Time: 29 ms (47.43%) | Memory: 32.4 MB (52.32%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -9,20 +9,60 @@
  */
 class Codec {
 public:
-    static TreeNode* t;
-
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        t = root;
-        return "";
+    if (!root) return "";
+    queue<TreeNode*> q;
+    q.push(root);
+    string serialized;
+
+    while (!q.empty()) {
+        TreeNode* node = q.front(); q.pop();
+        if (node) {
+            serialized += to_string(node->val) + ",";
+            q.push(node->left);
+            q.push(node->right);
+        } else {
+            serialized += "#,";
+        }
     }
+    return serialized;
+}
+
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        return t;
+    if (data.empty()) return nullptr;
+    stringstream ss(data);
+    string token;
+    getline(ss, token, ',');
+    TreeNode* root = new TreeNode(stoi(token));
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* node = q.front(); q.pop();
+
+        // Left child
+        if (getline(ss, token, ',')) {
+            if (token != "#") {
+                node->left = new TreeNode(stoi(token));
+                q.push(node->left);
+            }
+        }
+
+        // Right child
+        if (getline(ss, token, ',')) {
+            if (token != "#") {
+                node->right = new TreeNode(stoi(token));
+                q.push(node->right);
+            }
+        }
     }
+    return root;
+}
 };
-TreeNode* Codec::t = nullptr;
+
 // Your Codec object will be instantiated and called as such:
 // Codec ser, deser;
 // TreeNode* ans = deser.deserialize(ser.serialize(root));
