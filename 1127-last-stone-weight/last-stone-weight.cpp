ea2ab5268c78badb84163e7e3919f2e35bc49ad3
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-08-09 12:57:57 +0500

    Time: 5 ms (0.69%) | Memory: 9.8 MB (75.93%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(auto &it : stones) pq.push(it);
+        while(pq.size() > 1){
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+            cout<<x<<"  "<<y<<endl;
+
+            if(x != y){
+                y = x - y;
+                pq.push(y);
+            }
+        }
+        return !pq.empty() ? pq.top() : 0;
+    }
+};
