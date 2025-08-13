ed31b83074686629bc19ce55f9fdfeaeb582498e
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-08-13 15:43:58 +0500

    Time: 2 ms (5.23%) | Memory: 8.5 MB (52.25%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,10 +5,13 @@ public:
         return helper(n);
     }
     int helper(int n){
-        if(n == 0) return 1;
-        if(n < 0) return 0;
         if(mp.count(n)) return mp[n];
-        mp[n] = helper(n-1) + helper(n-2);
+        if(n == 0) return 1;
+
+        if(n < 0) return 0;
+        mp[n] =  helper(n-1)+helper(n-2);
         return mp[n];
     }
+    
+
 };
