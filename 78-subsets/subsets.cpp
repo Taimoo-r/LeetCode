28b51d7e2e50e7e47075b691c9012f4efd195eb3
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-08-25 13:59:02 +0500

    Time: 3 ms (31.74%) | Memory: 14.1 MB (24.38%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,50 +1,24 @@
-// class Solution {
-// public:
-//     vector<vector<int>> subsets(vector<int>& nums) {
-//         vector<vector<int>> ans;
-//         vector<int> curr;
-//         set<vector<int>> st;
-//         gen(nums, ans, curr, 0, st);
-//         return ans;
-//     }
-//     void gen(vector<int> &nums, vector<vector<int>> &ans, vector<int> &curr, int i, set<vector<int>> &st){
-//         if(i >= nums.size()) return;
-//         curr.push_back(nums[i]);
-//         if(!st.count(curr)){
-//         ans.push_back(curr);
-//         st.insert(curr);
-//         }
-//         gen(nums, ans, curr, i+1, st);
-//         curr.pop_back();
-//         if(!st.count(curr)){
-//         ans.push_back(curr);
-//         st.insert(curr);
-//         }
-//         gen(nums, ans, curr, i+1, st);
-
-//     }
-// };
-
 class Solution {
 public:
+    set<vector<int>> st;
+    vector<vector<int>> ans;
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
         vector<int> curr;
-        gen(nums, ans, curr, 0);
-        ans.push_back({}); // Include the empty subset explicitly
+        help(nums, 0, curr);
+        ans.push_back({});
         return ans;
     }
-    
-    void gen(vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, int i) {
-        if (i == nums.size()) return;
-        
-        // Include nums[i]
+    void help(vector<int>& nums, int i, vector<int>& curr){
+        if(i >= nums.size()) return;
+
         curr.push_back(nums[i]);
-        ans.push_back(curr);
-        gen(nums, ans, curr, i + 1);
-        
-        // Exclude nums[i]
+
+        if(!st.count(curr)) {
+            ans.push_back(curr);
+            st.insert(curr);
+        }
+        help(nums, i+1, curr);
         curr.pop_back();
-        gen(nums, ans, curr, i + 1);
+        help(nums, i+1, curr);
     }
 };
