073341e211e71163c994050052f4cf9d68b9bb53
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-08-26 10:44:53 +0500

    Time: 111 ms (5.01%) | Memory: 80 MB (5.05%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,32 +1,23 @@
-#pragma GCC optimize("O3")
-#pragma GCC target("arch=znver3")
-namespace _xd {
-    static const auto _ = [] {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        return 0;
-    }();
-}
 class Solution {
 public:
-    vector<vector<int>> res;
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        vector<int> currCom;
-        helper(nums,target,0,0, currCom);
-        return res;
-        
+    // set<vector<int>> st;
+    vector<vector<int>> ans;
+    vector<vector<int>> combinationSum(vector<int>& cand, int t) {
+        vector<int> curr;
+        help(cand, t, 0, curr, 0);
+        return ans;
     }
-    void helper(vector<int> &nums, int target, int index, int sum, vector<int>& comb){
-        if(sum==target){
-            res.push_back(comb);
-            return;
+
+    void help(vector<int>& cand, int t, int i, vector<int> curr, int sum){
+        if(i >= cand.size() || sum > t) return;
+        curr.push_back(cand[i]);
+        if(sum+cand[i] == t){
+            ans.push_back(curr);
         }
-        if(sum > target || index >= nums.size()){
-            return;
-        }
-        comb.push_back(nums[index]);
-        helper(nums, target, index,sum + nums[index], comb);
-        comb.pop_back();
-        helper(nums,target, index + 1, sum , comb);
+
+        help(cand, t, i, curr,sum+cand[i]);
+        curr.pop_back();
+        help(cand, t, i+1, curr, sum);
+
     }
 };
