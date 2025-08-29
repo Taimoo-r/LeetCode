0a32919820246bda40187ba3284b0f3e54848be7
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-08-29 10:21:04 +0500

    Time: 75 ms (31.71%) | Memory: 82.5 MB (42.12%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> ans;
+
     vector<vector<int>> combine(int n, int k) {
         vector<int> curr;
-        for(int i = 1 ; i <= n ; i++){
-            help(n, k, curr, i);
-        }
+        help(n, k, 1, curr);
         return ans;
     }
-    void help(int n, int k, vector<int> curr, int i){
-        if(i > n || curr.size() > k) return;
 
-        curr.push_back(i);
-
-        if(curr.size() == k){
+    void help(int n, int k, int start, vector<int>& curr) {
+        if (curr.size() == k) {
             ans.push_back(curr);
             return;
         }
 
-        for(int j = i ; j <= n ; j++){
-            help(n, k, curr, j+1);
-            // curr.pop_back();
+        for (int i = start; i <= n; i++) {
+            curr.push_back(i);
+            help(n, k, i + 1, curr);
+            curr.pop_back(); // backtrack
         }
     }
 };
