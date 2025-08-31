b7a3a4a612cc9294d9a5c95e21496692f17ee5e0
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-08-31 11:50:57 +0500

    Time: 195 ms (5.59%) | Memory: 35 MB (6.72%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+    unordered_set<int> st;
+    set<vector<int>> s;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        help(nums, 0);
+        return ans;
+    }
+    void help(vector<int> &nums, int i){
+        if(curr.size() == nums.size()  && !s.count(curr)){
+            ans.push_back(curr);
+            s.insert(curr);
+            return;
+        }
+
+        for(int i = 0 ; i < nums.size() ; i++){
+            
+            if(st.count(i)) continue;
+            st.insert(i);
+            curr.push_back(nums[i]);
+            help(nums, 0);
+            curr.pop_back();
+            st.erase(i);
+        }
+    }
+};
