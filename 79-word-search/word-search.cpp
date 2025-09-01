12b659850218214afd08f12865eec21e123c3934
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-09-01 10:06:14 +0500

    Time: 903 ms (11.63%) | Memory: 11.7 MB (34.19%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int rowSize = board.size();
-        int colSize = board[0].size();
-        for(int row = 0 ; row < rowSize; row++){
-            for(int col = 0 ; col < colSize ; col++){
-                if(word[0]==board[row][col]){
-                    if(helper(board, word, row, col, 0))
-                    return true;
+        for(int r = 0 ; r < board.size() ; r++){
+            for(int c = 0 ; c < board[r].size() ; c++){
+                if(board[r][c] == word[0]){
+                    if(help(board, word, r, c))
+                        return true;
                 }
             }
         }
         return false;
     }
-    bool helper(vector<vector<char>>& board, string& word, int row, int col, int index){
-        if(index == word.size())
-        return true;
-        if(row >= board.size() || row < 0 || col >= board[0].size() || col < 0 || board[row][col]!=word[index])
-        return false;
-        if(board[row][col]=='#')
-        return false;
-        board[row][col]= '#';
-        if( helper(board, word, row - 1, col, index + 1)
-            || helper(board, word, row + 1, col, index + 1)
-            || helper(board, word, row, col - 1, index + 1)
-            || helper(board, word, row, col + 1, index + 1))
-            return true;
-        board[row][col] = word[index];
-        return false;
+
+    bool help(vector<vector<char>>& board, string word, int r, int c){
+        if(word.size() == 0) return true;
+        if(r < 0 || c < 0 || r >= board.size() || c >= board[r].size()) return false;
+        if(board[r][c] == '#') return false;
+        if(board[r][c] != word[0]) return false;
+
+        int original = board[r][c];
+        board[r][c] = '#';
+
+        bool left = help(board, word.substr(1), r, c - 1);
+        bool right = help(board, word.substr(1), r, c + 1);
+        bool up = help(board, word.substr(1), r - 1, c);
+        bool down = help(board, word.substr(1), r + 1, c);
+
+        board[r][c] = original;
+        return (left || right || up || down);
     }
 };
