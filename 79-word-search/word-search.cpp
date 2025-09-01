fbc6a16e1e8ddf9de64f395f2e73545e266fb73a
Muhammad Taimoor <taimoorking23102004@gmail.com>
2025-09-01 10:19:54 +0500

    Time: 235 ms (68.58%) | Memory: 10.5 MB (73.65%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -4,7 +4,7 @@ public:
         for(int r = 0 ; r < board.size() ; r++){
             for(int c = 0 ; c < board[r].size() ; c++){
                 if(board[r][c] == word[0]){
-                    if(help(board, word, r, c))
+                    if(help(board, word, r, c, 0))
                         return true;
                 }
             }
@@ -12,19 +12,19 @@ public:
         return false;
     }
 
-    bool help(vector<vector<char>>& board, string word, int r, int c){
-        if(word.size() == 0) return true;
+    bool help(vector<vector<char>>& board, string& word, int r, int c, int i){
+        if(i >= word.size()) return true;
         if(r < 0 || c < 0 || r >= board.size() || c >= board[r].size()) return false;
         if(board[r][c] == '#') return false;
-        if(board[r][c] != word[0]) return false;
+        if(board[r][c] != word[i]) return false;
 
         int original = board[r][c];
         board[r][c] = '#';
 
-        bool left = help(board, word.substr(1), r, c - 1);
-        bool right = help(board, word.substr(1), r, c + 1);
-        bool up = help(board, word.substr(1), r - 1, c);
-        bool down = help(board, word.substr(1), r + 1, c);
+        bool left = help(board, word, r, c - 1, i+1);
+        bool right = help(board, word, r, c + 1, i+1);
+        bool up = help(board, word, r - 1, c, i+1);
+        bool down = help(board, word, r + 1, c, i+1);
 
         board[r][c] = original;
         return (left || right || up || down);
