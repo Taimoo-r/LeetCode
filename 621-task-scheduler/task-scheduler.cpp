8b26f723fc4810236119f9864908f4b01ead8215
Muhammad Taimoor <taimoorking23102004@gmail.com>
2026-03-18 11:26:57 +0500

    Time: 12 ms (57.78%) | Memory: 38.4 MB (68.70%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,35 +1,26 @@
+struct cmp{
+    bool operator()(pair<char, int> a, pair<char, int> b){
+        return a.second < b.second; 
+    }
+};
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         unordered_map<char, int> mp;
-        for(auto &it : tasks) mp[it]++;
-
-        priority_queue<int> pq;
-
-        for(auto [a, b] : mp) pq.push(b);
-
-        int time = 0;
+        for(auto &it: tasks) mp[it]++;
+        priority_queue<pair<char, int>, vector<pair<char, int>>, cmp> pq;
+        for(auto &[e, f]: mp) pq.push({e, f});
+        auto [e, f] = pq.top(); pq.pop();
+        // cout<<e<<" "<<f;
+        int t = f;
+        int I = (f - 1)*n;
         while(!pq.empty()){
-            int coolDownCycle = n +1;//because you can't do t + n, it should be t+n+1 so that there is 
-            // atleast n gap between two occurences of same task
-            vector<int> v;
-            while(coolDownCycle > 0 && !pq.empty()){
-                int count = 0;
-                if(!pq.empty()){
-                    count = pq.top();
-                    pq.pop();
-                }
-                if(count - 1 > 0) v.push_back(count -1);
-                coolDownCycle--;
-                time++;
-            }
-
-            for(auto &it : v){
-                pq.push(it);
-            }
-
-            if(!pq.empty()) time+=coolDownCycle;
+            auto [k, o] = pq.top(); pq.pop();
+            // cout<<"\n"<<k<<" "<<o;
+            I-=(o > t-1 ? t-1 : o);
+            // cout<<" I = "<<I<<endl;
         }
-        return time;
+        I=max(I,0);
+        return I+tasks.size();
     }
 };
