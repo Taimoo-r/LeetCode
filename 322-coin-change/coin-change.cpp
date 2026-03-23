5c0a8b7b0ebce6aa6e21ca0fa28c6da71767fa14
Muhammad Taimoor <taimoorking23102004@gmail.com>
2026-03-23 11:41:31 +0500

    Time: 260 ms (5.02%) | Memory: 65.6 MB (5.13%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);  // Initialize the dp array
-        dp[0] = 0;  // Base case: no coins are needed for amount 0
+    int mini = INT_MAX;
+    unordered_map<int, int> memo;
 
-        // Iterate over each amount from 1 to target amount
-        for (int i = 1; i <= amount; ++i) {
-            for (int coin : coins) {
-                if (i - coin >= 0 && dp[i - coin] != INT_MAX) {
-                    dp[i] = min(dp[i], dp[i - coin] + 1);
-                }
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(), coins.end());
+        int ans = rec(coins, amount);
+        return ans == INT_MAX ? -1 : ans;
+    }
+
+    int rec(vector<int>& c, int a){
+        if(a == 0) return 0;
+        if(memo.count(a)) return memo[a];
+
+        int res = INT_MAX;
+
+        for(auto &x : c){
+            if(a - x < 0) break;
+
+            int sub = rec(c, a - x);
+            if(sub != INT_MAX){
+                res = min(res, sub + 1);
             }
         }
 
-        // If dp[amount] is still INT_MAX, it means we cannot make up the amount
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
+        return memo[a] = res;
     }
 };
