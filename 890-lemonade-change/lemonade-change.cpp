1afd12b31853a649526d536fa9cdf99697200a5c
Muhammad Taimoor <taimoorking23102004@gmail.com>
2026-03-24 11:04:09 +0500

    Time: 4 ms (5.41%) | Memory: 87.5 MB (11.30%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int change = 0;
-        map<int,int> mp;
-        for(int i = 0 ; i < bills.size(); i++){
-            int netPay = bills[i];
-            if(bills[i]==5){
-                mp[5]++;
-            }
-            else if(bills[i]==10){
-                if(mp[5]!=0){
-                    mp[10 - 5]--;
+        unordered_map<int, int> mp;
+        for(auto &x: bills){
+            if(x == 5) mp[x]++;
+
+            else if(x == 10){
+                if(mp[5]==0) return false;
+                else{
+                    mp[5]--;
                     mp[10]++;
                 }
-                else
-                return false;
             }
-            else if(bills[i]==20){
-                if(mp[10]!=0 && mp[5]!=0){
-                    mp[10]--;
-                    mp[5]--;
+            else if(x==20){
+                    if(mp[10] == 0 && mp[5] < 3) return false;
+                    if(mp[10] > 0 && mp[5] == 0) return false;
+                    if(mp[10] > 0) {
+                        mp[10]--;
+                        mp[5]--;
+                    } else {
+                        mp[5]-=3;
+                    }
+                    mp[20]++;
                 }
-                else if(mp[5]>=3){
-                    mp[5]-=3;
-                }
-                else return false;
             }
-            
-            
-        }
         return true;
-        
     }
 };
