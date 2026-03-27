5d8e255a88280f6eb096a4353a9b3ef0e0e7571d
Muhammad Taimoor <taimoorking23102004@gmail.com>
2026-03-27 11:56:58 +0500

    Time: 0 ms (100.00%) | Memory: 52.5 MB (24.97%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    vector<int> dp; // Memoization array
-
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        dp.resize(n, -1); // Initialize memoization array
-        return check(nums, 0);
-    }
-
-    bool check(vector<int>& nums, int index) {
-        if (index >= nums.size() - 1) return true;  // Base case: reached last index
-        if (nums[index] == 0) return false;         // Can't jump further
-        if (dp[index] != -1) return dp[index];      // Use memoized result
-
-        for (int i = nums[index]; i > 0; i--) { // Try all jumps from current index
-            if (check(nums, index + i)) {
-                return dp[index] = true; // Cache result and return true
-            }
+        if(nums.size() < 2) return true;
+        int curr = nums[0];
+        for(int i = 1 ; i < nums.size() ; i++){
+            curr--;
+            if(curr < 0) return false;
+            int x = nums[i];
+            if(curr < x) curr = x;
         }
-        return dp[index] = false; // Cache result and return false
+        return true;
     }
 };
