b66590d940b4be5210a16f4c654f14d4341139bb
Muhammad Taimoor <taimoorking23102004@gmail.com>
2026-04-12 13:54:45 +0500

    Time: 35 ms (95.06%) | Memory: 93.9 MB (67.44%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        // Sort intervals based on the end time (interval[1])
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];  // Sort by the second element (end time)
+    int eraseOverlapIntervals(vector<vector<int>>& in) {
+        sort(in.begin(), in.end(), [](auto &a, auto &b){
+            return a[1] < b[1]; // sort by end time
         });
 
-        int overlapCount = 0;
-        int maxY = intervals[0][1];  // Keep track of the end of the last added interval
-        
-        for (int i = 1; i < intervals.size(); i++) {
-            int x = intervals[i][0];
-            int y = intervals[i][1];
-            
-            if (x < maxY) {
-                // Overlap occurs, increment the count
-                overlapCount++;
+        int cnt = 0;
+        int prevEnd = in[0][1];
+
+        for(int i = 1; i < in.size(); i++){
+            if(in[i][0] < prevEnd){
+                // overlap → remove this interval
+                cnt++;
             } else {
-                // No overlap, update maxY to the end of the current interval
-                maxY = y;
+                // no overlap → update prevEnd
+                prevEnd = in[i][1];
             }
         }
-        return overlapCount;
+
+        return cnt;
     }
 };
